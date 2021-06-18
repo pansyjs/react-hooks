d670a34d3b46a746e870e669deb1caa5b8ff9fd8
wangxingkang <wang_xingkang@qq.com>
2021-06-18 18:33:25 +0800

    chore: 解决编译问题

---
+++ .walrusrc.ts
@@ -0,0 +1,9 @@
+import { Config } from '@walrus/types';
+
+const config: Config = {
+  entry: {
+    format: 'littleCamelCase'
+  }
+};
+
+export default config;
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// 此文件在构建时会自动更新，请勿手动修改! 
+import useBoolean from './useBoolean';
+import useModal from './useModal';
+import useToggle from './useToggle';
+
+export {
+  useBoolean,
+  useModal,
+  useToggle
+};
+
+export default {
+  useBoolean,
+  useModal,
+  useToggle
+};
