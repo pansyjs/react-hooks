895ece6527e9fdeb28eb0523ddc13c812c19cb52
wangxingkang <156148958@qq.com>
2021-12-19 22:24:26 +0800

    🚧 chore: add getTargetElement utils fun

---
+++ src/utils/domTarget.ts
@@ -0,0 +1,39 @@
+import type { MutableRefObject } from 'react';
+import { isBrowser } from './isBrowser';
+
+type TargetValue<T> = T | undefined | null;
+
+type TargetType = HTMLElement | Element | Window | Document;
+
+export type BasicTarget<T extends TargetType = Element> =
+  | (() => TargetValue<T>)
+  | TargetValue<T>
+  | MutableRefObject<TargetValue<T>>;
+
+/**
+ * 获取Dom节点
+ * @param target
+ * @param defaultElement
+ * @returns
+ */
+export function getTargetElement<T extends TargetType>(target: BasicTarget<T>, defaultElement?: T) {
+  if (!isBrowser) {
+    return undefined;
+  }
+
+  if (!target) {
+    return defaultElement;
+  }
+
+  let targetElement: TargetValue<T>;
+
+  if (typeof target === 'function') {
+    targetElement = target();
+  } else if ('current' in target) {
+    targetElement = target.current;
+  } else {
+    targetElement = target;
+  }
+
+  return targetElement;
+}
