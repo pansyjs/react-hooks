b267a28ac62900f9d7d10de5544541c77c7b52db
wangxingkang <wang_xingkang@qq.com>
2022-04-07 11:24:40 +0800

    📝 docs: 文档目录优化

--- .umirc.ts
+++ .umirc.ts
@@ -1,4 +1,14 @@
 import { join } from 'path';
+import { readdirSync } from 'fs';
+
+const headPkgList = [];
+const pkgList = readdirSync(join(__dirname, 'hooks')).filter(
+  (pkg) => pkg.charAt(0) !== '.' && !headPkgList.includes(pkg),
+);
+
+const tailPkgList = pkgList
+  .map((path) => [join('hooks', path, 'docs')])
+  .reduce((acc, val) => acc.concat(val), []);
 
 const logo = 'https://cdn.jsdelivr.net/gh/wangxingkang/pictures@latest/imgs/20210623172110.svg';
 
@@ -17,11 +27,86 @@ export default {
   ],
   resolve: {
     includes: [
-      "docs"
-    ]
+      ...tailPkgList,
+      'docs'
+    ],
   },
   alias: {
     '@pansy/react-hooks': join(__dirname, 'hooks/hooks/src'),
+  },
+  menus: {
+    '/hooks': [
+      {
+        title: 'Scene',
+        children: [
+          'useModal.md',
+          'useMqtt.md',
+          'useSelections.md',
+          'useWebSocket.md',
+        ],
+      },
+      {
+        title: 'LifeCycle',
+        children: [
+          'useUnmount.md',
+        ],
+      },
+      {
+        title: 'State',
+        children: [
+          'useBoolean.md',
+          'useGetState.md',
+          'useLocalStorageState.md',
+          'useRafState.md',
+          'useSessionStorageState.md',
+          'useToggle.md',
+        ],
+      },
+      {
+        title: 'LifeCycle',
+        children: [
+          'useUnmount.md',
+        ],
+      },
+      {
+        title: 'Effect',
+        children: [
+          'useInterval.md',
+          'useLockFn.md',
+          'useTimeout.md',
+          'useUpdate.md',
+          'useUpdateEffect.md',
+          'useUpdateLayoutEffect.md',
+        ],
+      },
+      {
+        title: 'Dom',
+        children: [
+          'useClickAway.md',
+          'useDocumentVisibility.md',
+          'useEventListener.md',
+          'useEventTarget.md',
+          'useExternal.md',
+          'useFavicon.md',
+          'useHover.md',
+          'useInViewport.md',
+          'useKeyPress.md',
+          'useScroll.md',
+          'useSize.md',
+          'useTitle.md',
+          'useWatermark.md',
+        ],
+      },
+      {
+        title: 'Advanced',
+        children: [
+          'useControllableValue.md',
+          'useLatest.md',
+          'useMemoizedFn.md',
+          'usePrevious.md',
+        ],
+      },
+    ]
   },
   hash: true,
   extraBabelPlugins: [
---
+++ hooks/useUpdateEffect/docs/useUpdateEffect.md
@@ -0,0 +1,29 @@
+---
+title: useUpdateEffect
+nav:
+  title: Hooks
+  path: /hooks
+group:
+  path: /
+---
+
+# useUpdateEffect
+
+`useUpdateEffect` 用法等同于 `useEffect`，但是会忽略首次执行，只在依赖更新时执行。
+
+## 代码演示
+
+### 基础用法
+
+<code src="./demo/demo01.tsx" />
+
+## API
+
+API 与 `React.useEffect` 完全一致。
+
+```typescript
+useUpdateEffect(
+  effect: React.EffectCallback,
+  deps?: React.DependencyList,
+)
+```
